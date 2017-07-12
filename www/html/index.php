07cae21c2c064e7c667b79ea737177335165d5b0
brunonra <bruno.araujo.sevice@gmail.com>
2017-07-12 13:35:16 -0300

    Update index.php

--- www/html/index.php
+++ www/html/index.php
@@ -1,14 +1,14 @@
 <html>
-<head> 
-<title> Lista de APPs </title>
-</head>
+  <head> <title> No Life DEV </title></head>
+  <h1>Bem vindo a nossa pagina de APPs. </h1>
 
+  <p>Aplicativo de dicas para Magic the Gathering, para ver clique na foto! </p>  
+  <p><a href="/magic/magic.php">Magic App </a></p>
+  <p><a href="/magic/magic.php"> <img src="img/dica01.jpg". height='60' width='60'. alt="Teste IMG"/> </a>
 
-<p><a href="/magic/magic.php">Magic App </a></p>
-<p><a href="/magic/magic.php"> <img src="img/dica01.jpg". height='60' width='60'. alt="Teste IMG"/> </a>
-
-<p> <a href="/skull/skull.html">Skull Game </a> </p>
-<p><a href="/skull/skull.html"> <img src="img/skull.jpg". height='60' width='60' . alt="Teste IMG"/> </a>
+  <p>Nosso Jogo! Skull Invaders! Clique na foto e divirta-se!</p>
+  <p> <a href="/skull/skull.html">Skull Game </a> </p>
+  <p><a href="/skull/skull.html"> <img src="img/skull.jpg". height='60' width='60' . alt="Teste IMG"/> </a>
 
 
 </html>
