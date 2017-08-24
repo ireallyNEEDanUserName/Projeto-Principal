3eaf4035a6e0ed325e3b1706db7abbe24a1add8e
Felipe Wieler <felipe.a.wieler@gmail.com>
2017-08-24 17:59:06 -0300

    Update conf.js

--- atualizada 3.0/html/conf.js
+++ atualizada 3.0/html/conf.js
@@ -1,7 +1,18 @@
 $(document).ready(function(){
+
 	$('h3').click(function(){
-		var href = $(this).attr('href');
-		$('#centro').load( href );
+		var isso = $(this);
+		clicar(isso);
 		$(this).css({ 'color': 'blue', 'text-decoration': 'underline'});
 	})
+	$('li1').click(function(){
+		var isso = $(this);
+		clicar(isso);
+	})
 });
+
+
+var clicar = function(isso){
+	var href = isso.attr('href');
+	$('#centro').load( href );
+};
