ccd9ec25e716984323ebe84ead406269521f57f0
Felipe Wieler <felipe.a.wieler@gmail.com>
2017-08-24 20:56:49 -0300

    Add files via upload

---
+++ atualizada 3.0/html/mobile.js
@@ -0,0 +1,17 @@
+var userAgent = navigator.userAgent.toLowerCase();
+var devices = new Array('nokia','iphone','blackberry','sony','lg',
+	'htc_tattoo','samsung','symbian','SymbianOS','elaine','palm',
+	'series60','windows ce','android','obigo','netfront',
+	'openwave','mobilexplorer','operamini');
+var url_redirect = 'm.index.html';
+function mobiDetect(userAgent, devices) {
+	for(var i = 0; i < devices.length; i++) {
+		if (userAgent.search(devices[i]) > 0) {
+			return true;
+		}
+	}
+	return false;
+}
+if (mobiDetect(userAgent, devices)) {
+	window.location.href = url_redirect;
+}
