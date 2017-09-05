1908a9cd9e38050004205ad130a160c5a6d50d5a
Felipe Wieler <felipe.a.wieler@gmail.com>
2017-09-04 22:58:02 -0300

    Update ranking.php

--- v3.0/html/skull/ranking.php
+++ v3.0/html/skull/ranking.php
@@ -7,19 +7,8 @@
 
 <body>
 	
-	<table> 
-		<tr>
-			<td>RANKING DOS JOGADORES</td>
-		</tr>
-		<tr>
-			<th>Nome</th>
-			<th>Fase</th>
-			<th>Local</th>
-		</tr>
+	<?php include 'rankingBanco.php'; ?>	
 
-		<?php include 'rankingBanco.php'; ?>	
-
-	</table>
 </body>
 
 </html>
