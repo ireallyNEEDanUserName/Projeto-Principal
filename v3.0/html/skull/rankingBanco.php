b4a42712525158c7f063128d54e1d3c057bed6e9
Felipe Wieler <felipe.a.wieler@gmail.com>
2017-09-04 22:57:38 -0300

    Update rankingBanco.php

--- v3.0/html/skull/rankingBanco.php
+++ v3.0/html/skull/rankingBanco.php
@@ -5,20 +5,48 @@
 	}
 	else {
 
-   		$sql = "SELECT * FROM ranking ORDER BY level desc"; 
+   		$sql = "SELECT * FROM ranking WHERE versao='m' ORDER BY level desc"; 
+   		$pSql = "SELECT * FROM ranking WHERE versao='p' ORDER BY level desc"; 
+   		
    		$result = pg_query($conexao, $sql); 
+   		$resultP = pg_query($conexao, $pSql);
    
 		$all = pg_fetch_all($result);
+		$allP = pg_fetch_all($resultP);
 		
-
+		echo "<table>";
+		echo "<th colspan = " . "2 " .">RANKING DOS JOGADORES MOBILE</th>";
+		echo "<tr>
+				<th>Nome</th>
+				<th>Fase</th>
+			</tr>";
 		for($i = 0; $i < count($all); $i++){
 			echo "<tr>";
 			foreach($all[$i] as $key => $value) {
-				echo "<td>" . $value . "</td>";
+				if($value !== 'm') echo "<td>" . $value . "</td>";
 			}
 			echo "</tr>";
-
 		}
+		echo "<table><br>";
+
+
+		echo "<br>";
+		echo "<table>";
+		echo "<th colspan = " . "2 " .">RANKING DOS JOGADORES PC</th>";
+		echo "<tr>
+				<th>Nome</th>
+				<th>Fase</th>
+			</tr>";
+
+		for($i = 0; $i < count($allP); $i++){
+			echo "<tr>";
+			foreach($allP[$i] as $key => $value) {
+				if($value !== 'p') echo "<td>" . $value . "</td>";
+			}
+			echo "</tr>";
+		}
+		echo "</table>";
+
    		pg_close($conexao); 
 	}
 
