813b9222a3c5a2c9b70c7782546daf07484a82d5
Felipe Wieler <felipe.a.wieler@gmail.com>
2017-09-04 22:08:26 -0300

    Update enviarBanco.php

--- v3.0/html/skull/enviarBanco.php
+++ v3.0/html/skull/enviarBanco.php
@@ -4,9 +4,10 @@ $connect = pg_connect($conn_str);
 
 $nome = $_POST['nome'];
 $fase = $_POST['fase'];
+$tipo = $_POST['tipo'];
 
 if($fase > 1){
-	$query = "INSERT INTO ranking VALUES ('$nome', $fase)";
+	$query = "INSERT INTO ranking VALUES ('$nome', $fase, '$tipo')";
 
 	$qResult = pg_query($connect, $query);
 }
