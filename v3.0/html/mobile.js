0a4ad77df87ac2b23e1a63c0ef119a20ee102c8e
Felipe Wieler <felipe.a.wieler@gmail.com>
2017-09-06 18:00:47 -0300

    Update mobile.js

--- v3.0/html/mobile.js
+++ v3.0/html/mobile.js
@@ -1,9 +1,18 @@
+var achar = function(texto){
+	var ultimo = 0;
+	for(var x = 0; x < texto.length; x++){
+		if("/" == texto[x]) ultimo = x;
+	}
+	return ultimo + 1;
+}
+
+
 var userAgent = navigator.userAgent.toLowerCase();
 var devices = new Array('nokia','iphone','blackberry','sony','lg',
 	'htc_tattoo','samsung','symbian','SymbianOS','elaine','palm',
 	'series60','windows ce','android','obigo','netfront',
 	'openwave','mobilexplorer','operamini');
-var url_redirect = 'm.index.html';
+
 function mobiDetect(userAgent, devices) {
 	for(var i = 0; i < devices.length; i++) {
 		if (userAgent.search(devices[i]) > 0) {
@@ -13,6 +22,10 @@ function mobiDetect(userAgent, devices) {
 	return false;
 }
 if (mobiDetect(userAgent, devices)) {
-	if(window.location.pathname == "index.html") window.location.href = url_redirect;
-	else if(window.location.pathname == "/skull/skull.html") window.location.pathname = "/skull/m.skull.html";
+	if(!window.location.href.includes("m.")){
+		var url_redirect = "m." + window.location.pathname.slice(achar(window.location.pathname));
+		//console.log(url_redirect);
+		window.location.href = url_redirect;
+	}
 }
+
