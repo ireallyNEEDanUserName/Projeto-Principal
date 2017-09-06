06b6c0b33c5ca49f8899ec01b037bda4af385836
Felipe Wieler <felipe.a.wieler@gmail.com>
2017-09-06 17:16:39 -0300

    Update mobile.js

--- v3.0/html/mobile.js
+++ v3.0/html/mobile.js
@@ -13,5 +13,5 @@ function mobiDetect(userAgent, devices) {
 	return false;
 }
 if (mobiDetect(userAgent, devices)) {
-	if(window.location.href == "index.html") window.location.href = url_redirect;
+	if(window.location.pathname == "index.html") window.location.href = url_redirect;
 }
