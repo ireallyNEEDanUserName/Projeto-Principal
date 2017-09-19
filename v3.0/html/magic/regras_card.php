b1dd4116bdeac708f04b69c6a181b0c8f5d7e055
Felipe Wieler <felipe.a.wieler@gmail.com>
2017-09-19 12:01:06 -0300

    Update regras_card.php

--- v3.0/html/magic/regras_card.php
+++ v3.0/html/magic/regras_card.php
@@ -1,19 +1,22 @@
-<html>	<title>Efeitos dos Cards</title>
-
-	<link href="../logo.css" type="text/css" rel="stylesheet" />
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link href="cardo.css" type="text/css" rel="stylesheet" media="all" />
-	<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
-	<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.4/angular.min.js"></script>
+<html>	
 	
-	<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-<script>
-  (adsbygoogle = window.adsbygoogle || []).push({
-  google_ad_client: "ca-pub-9731344339248002",
-  enable_page_level_ads: true
-  });
-</script> 
+	<head>
+		<title>Efeitos dos Cards</title>
 
+		<link href="../logo.css" type="text/css" rel="stylesheet" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link href="cardo.css" type="text/css" rel="stylesheet" media="all" />
+		<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+		<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.4/angular.min.js"></script>
+		<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+		<script>
+  			(adsbygoogle = window.adsbygoogle || []).push({
+  			google_ad_client: "ca-pub-9731344339248002",
+  			enable_page_level_ads: true
+  			});
+		</script> 
+	</head>
+	
 	<body ng-app="">
 		<div id="topo">
 			<h1 ALIGN="center">Efeitos dos cards e o que significam. </h1>
