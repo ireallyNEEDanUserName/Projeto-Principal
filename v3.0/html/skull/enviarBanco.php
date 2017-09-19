df4ada15b22f2dfae52f7203f57337e77fc51e42
Felipe Wieler <felipe.a.wieler@gmail.com>
2017-09-19 14:21:39 -0300

    Update enviarBanco.php

--- v3.0/html/skull/enviarBanco.php
+++ v3.0/html/skull/enviarBanco.php
@@ -1,16 +1,17 @@
 <?php
-$conn_str = "host=localhost port=5432 dbname=skull user=skullusr password=S@k4lL!";
-$connect = pg_connect($conn_str);
+	$conn_str = "host=localhost port=5432 dbname=skull user=skullusr password=S@k4lL!";
+	$conn_str_Teste = "host=localhost port=5432 dbname=postgres user=postgres password=root";
+	$connect = pg_connect($conn_str);
 
-$nome = $_POST['nome'];
-$fase = $_POST['fase'];
-$tipo = $_POST['tipo'];
+	$nome = $_POST['nome'];
+	$fase = $_POST['fase'];
+	$tipo = $_POST['tipo'];
 
-if($fase > 1){
-	$query = "INSERT INTO ranking VALUES ('$nome', $fase, '$tipo')";
+	if($fase > 1){
+		$query = "INSERT INTO ranking VALUES ('$nome', $fase, '$tipo')";
 
-	$qResult = pg_query($connect, $query);
-}
-pg_close($connect);
+		$qResult = pg_query($connect, $query);
+	}
+	pg_close($connect);
 
 ?>
