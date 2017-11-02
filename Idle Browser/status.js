8e8ba54efab1ac817a6d5f18f49bbfc7c559f8bd
Felipe Wieler <felipe.a.wieler@gmail.com>
2017-11-01 22:09:27 -0300

    Update status.js

--- Idle Browser/status.js
+++ Idle Browser/status.js
@@ -4,6 +4,10 @@ var start = function(){
 	status = iniciar(status);
 	escrever(status);
 	
+	window.addEventListener("beforeunload", function(){
+		localStorage.setItem("Jogador", JSON.stringify(status));
+	});
+	
 };
 
 var escrever = function(status){
@@ -12,9 +16,16 @@ var escrever = function(status){
 	
 	var str = "";
 	var cap = "";
+	var tam = 0;
 	for(var key in status){
-		cap = key.charAt(0).toUpperCase() + key.slice(1);
-		str += " | " + cap + ": " + status[key] + " | ";
+		if(key == 'inventario'){
+			for(var chave in status[key]) tam += status[key][chave];			
+			cap = key.charAt(0).toUpperCase() + key.slice(1);
+			str += " | " + cap + ": " + tam + " | ";
+		}else{
+			cap = key.charAt(0).toUpperCase() + key.slice(1);
+			str += " | " + cap + ": " + status[key] + " | ";
+		}
 	}
 	
 	dados.innerHTML = str;
