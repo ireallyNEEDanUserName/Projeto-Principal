6dd87192ac6bf1bfa8b1dceeceabac21b1283024
Felipe Wieler <felipe.a.wieler@gmail.com>
2017-11-04 21:57:14 -0300

    Update status.js

--- v3.0/html/IdleBrowser/status.js
+++ v3.0/html/IdleBrowser/status.js
@@ -22,6 +22,19 @@ var escrever = function(status){
 			for(var chave in status[key]) tam += status[key][chave];			
 			cap = key.charAt(0).toUpperCase() + key.slice(1);
 			str += " | " + cap + ": " + tam + " | ";
+		}else if(key.includes("exp")){
+		
+			var texto = "";
+			if(key.includes("Minerar")) texto = "Minerar";	
+			else if(key.includes("Forjar")) texto = "Forjar";
+			else if(key.includes("Caca")) texto = "Cacar";
+
+			var level = "lvl" + texto;
+			var experiencia = "exp" + texto; 
+			var compExp = (status[level] * status[level]) * (50 + status[level]);
+			
+			cap = key.charAt(0).toUpperCase() + key.slice(1);
+			str += " | " + cap + ": " + status[key] + " / " + compExp + " | ";
 		}else{
 			cap = key.charAt(0).toUpperCase() + key.slice(1);
 			str += " | " + cap + ": " + status[key] + " | ";
