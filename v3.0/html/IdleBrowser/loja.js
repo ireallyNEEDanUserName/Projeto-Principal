0c663255e234dcb8cb11273a3a624d505b5efacf
Felipe Wieler <felipe.a.wieler@gmail.com>
2017-11-16 21:30:53 -0300

    Update loja.js

--- v3.0/html/IdleBrowser/loja.js
+++ v3.0/html/IdleBrowser/loja.js
@@ -9,11 +9,29 @@ var addInv = function(tipo, qtd){
 
 	var status = {};
 	status = iniciar(status);
-
-	if(tipo == "pedra" || tipo == "ferro") tipoMaterial = "construcao";
-	if(tipo == "ferro") qtd *= 2;
-
-	status.inventario[tipoMaterial] += qtd;
+	
+	var custo;
+	var qtdGeral;
+	
+	if(tipo == "pedra"){
+		tipoMaterial = "minerio";
+		custo = 2;
+		qtdGeral = qtd;
+	}
+	else if(tipo == "ferro"){
+		tipoMaterial = "minerio";
+		custo = 5;
+		qtdGeral = qtd * 2;
+	}
+	
+	var preco = qtd * custo;
+	
+	if(status.inventario["dinheiro"] >= preco){
+		status.inventario[tipoMaterial] += qtdGeral;
+		status.inventario["dinheiro"] -= preco;
+		textoFinalPagina("Comprou com sucesso " + qtd + " de " + maiuscula(tipo) + " por: " + preco);
+	}
+	else textoFinalPagina("Dinheiro Insuficiente");	
 
 	salvar(status);
 	materiais();
