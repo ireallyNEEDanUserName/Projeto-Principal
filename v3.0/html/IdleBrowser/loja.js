f064b885a2b432c9ba5f3b6c4261e4d8ccc3745a
Felipe Wieler <felipe.a.wieler@gmail.com>
2017-11-17 21:25:59 -0300

    Update loja.js

--- v3.0/html/IdleBrowser/loja.js
+++ v3.0/html/IdleBrowser/loja.js
@@ -37,3 +37,47 @@ var addInv = function(tipo, qtd){
 	materiais();
 
 };
+
+
+
+/*
+
+<p id="itens">
+	<i class="comp" value="pedra"> COMPRAR </i> <a class="btn" value="pedra" id="-"> - </a>
+	<br>
+	<i> Ferro: </i> <i id="ferroQtd"> 1 </i> | Custa: <i id="ferroVal"> 5 </i> | <a class="btn" value="ferro" id="+"> + </a> <i class="comp" value="ferro"> COMPRAR </i> <a class="btn" value="ferro" id="-"> - </a>
+</p>
+		
+*/
+
+
+var criarBuy = function(){
+	
+	var itens = {}; 
+	itens = defItens(itens);
+	
+	var titulo = document.getElementById("titulo");	
+	
+	var div = "<p id='itens'>";
+	
+	for(var key in itens){
+		
+		div += "<h3> " + maiuscula(key) + " </h3>";
+		
+		for(var keys in itens[key]){
+			console.log(itens[key][keys]);
+			div += "<i>" +  maiuscula(keys) + ": </i>"+
+					"<i id=" + keys + "Qtd>  1 </i>" +
+					"| Custa: " +
+					"<i id=" + keys + "Val>" + itens[key][keys].buy + "</i> |" +
+					"<a class='btnCompra' value='" + keys + "' id='+' outro='" + key + "' > + </a>" +
+					"<i class='comp' value='" + keys + "'> COMPRAR </i>" +
+					"<a class='btnCompra' value='" + keys + "' id='-' outro='" + key + "'> - </a> <br>";
+		}
+	}
+	
+	div += "</p>";
+	
+	titulo.insertAdjacentHTML('afterend', div);
+	
+};
