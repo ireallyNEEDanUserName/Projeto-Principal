230b2641331c526b3c597715885bf581c528d8a1
Felipe Wieler <felipe.a.wieler@gmail.com>
2017-11-22 20:18:08 -0300

    Update status.js

--- v3.0/html/IdleBrowser/status.js
+++ v3.0/html/IdleBrowser/status.js
@@ -14,16 +14,14 @@ var escreverStatus = function(status){
 	var cap = "";
 	var tam = 0;
 	for(var key in status){
+		cap = maiuscula(key);
 		if(key == 'inventario'){
 			for(var chave in status[key]) tam += status[key][chave];			
-			cap = maiuscula(key);
 			str += " | " + cap + ": " + tam + " | ";
 		}else if(key == 'empregados'){
 			tam = Object.keys(status[key]).length;
-			cap = maiuscula(key);
 			str += " | " + cap + ": " + tam + " | ";
 		}else if(key.includes("exp")){
-		
 			var texto = "";
 			if(key.includes("Minerar")) texto = "Minerar";	
 			else if(key.includes("Forjar")) texto = "Forjar";
@@ -35,12 +33,13 @@ var escreverStatus = function(status){
 			var experiencia = "exp" + texto; 
 			var compExp = (status[level] * status[level]) * (50 + status[level]);
 			
-			cap = maiuscula(key);
-			str += " | " + cap + ": " + status[key] + " / " + compExp + " | ";
+			str += " | " + cap + ": " + status[key] + " / " + compExp + " | ";	
+		}else if(key.includes("combate") || key.includes("habilidades")){
+			console.log(status[key]);
 		}else{
-			cap = maiuscula(key);
 			str += " | " + cap + ": " + status[key] + " | ";
 		}
+		
 	}
 	
 	dados.innerHTML = str;
