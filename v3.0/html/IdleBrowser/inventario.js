491468d27804125a52dac23a6c41e7403646d818
Felipe Wieler <felipe.a.wieler@gmail.com>
2017-12-10 21:10:00 -0300

    Update inventario.js

--- v3.0/html/IdleBrowser/inventario.js
+++ v3.0/html/IdleBrowser/inventario.js
@@ -6,7 +6,7 @@ var impInv = function(){
 	var itens = {}; 
 	itens = defItens(itens);
 	
-	var ult = document.getElementById('barraInvt');
+	var ult = document.getElementById('Menu');
 	
 	console.log(status.inventario);
 	
@@ -14,29 +14,39 @@ var impInv = function(){
 	
 	for(var key in itens){
 		div += "<h3>" + maiuscula(key) + "</h3>";
-		for(var chave in itens[key]){
-			if(maiuscula(chave) in status.inventario) div += "<p>" + maiuscula(chave) + " : " + status.inventario[maiuscula(chave)]  + " </p>";
+		div += "<p>";
+		for(var chave in itens[key]){		
+			if(maiuscula(chave) in status.inventario) div += maiuscula(chave) + " : " + status.inventario[maiuscula(chave)] + " | ";	
 		}
+		div += " </p>";
 	}
 	
-	for(var key in status.inventario){
 	
-		if(status.inventario[key] == 0 || key.includes("+0")) delete status.inventario[key];
-		else{
-			item = verificarItem(minuscula(key));
-			try{
-				if(item.tipo == "forjar"){
-					div += "<p>" + key + " : " + status.inventario[key]  + " </p>";
+	
+	for(var chave in itens.forja){
+		div += "<p>" ;
+		for(var key in status.inventario){
+			if(key.includes(maiuscula(chave))){
+				if(status.inventario[key] == 0 || key.includes("+0")) delete status.inventario[key]; //Deleta os itens +0, somente item normal e +1 depois do primeiro refino.
+				else{
+					item = verificarItem(minuscula(key));
+					try{
+						if(item.tipo == "forjar"){
+							div += key + " : " + status.inventario[key] + " | ";
+						}
+					}catch(err){
+						console.log("Erro na pagina inventario.js na linha 31 - " + err);
+					}
 				}
-			}catch(err){
-				console.log("Erro na pagina inventario.js na linha 31 - " + err);
 			}
 		}
+		div += " </p>";
 	}
+	
 	
 	div += "</div>";
 	
-	ult.insertAdjacentHTML('beforeend', div);
+	ult.insertAdjacentHTML('afterend', div);
 	
 	
 	window.addEventListener("beforeunload", function(){
