22e261f8226b60031b66fe43c0b330112cdd245b
Felipe Wieler <felipe.a.wieler@gmail.com>
2018-03-02 16:27:06 -0300

    Update Ajuda codigo.php

--- Ajuda codigo.php
+++ Ajuda codigo.php
@@ -1,53 +1,29 @@
-<html>
-<body>
+<?php
+	
+	echo "Inicio: <br>";
+	$strBanco = "host=172.16.19.140 port=5432 dbname=portal_tst user=portalusr password=P0rtal!";
+	
+	if(!@($conexao = pg_connect($strBanco))){
+		print "Não foi possível estabelecer uma conexão com o banco de dados.";
+		echo "ERRO: <BR>";
+	}else{
+		$sql = "SELECT COUNT(IP) AS 'ATUALIZAR' FROM inv_srv WHERE obsolescencia='SIM'";
+		$result = pg_query($conexao, $sql);
+		$resultAll = pg_fetch_assoc($result);
+		
+		echo $resultAll['ATUALIZAR'];
+		/*
+		echo "Funciona";
+		
+		foreach($resultAll as $key){
+			echo "Hostname: " . $key['hostname'] . "<br>";
+			echo "IP: " . $key['ip'] . "<br>"
+		}
+		*/
+		
+		pg_close($conexao);
+	}
+	
+	echo "FIM <br>";
 
-        <?php
-                echo "INICIIOOOOOOOo <br>";
-                $strBanco = "host=172.16.19.140 port=5432 bdname=portal_tst user=portalusr password=P0rta1!";
-
-
-                if(!@($conexao = pg_connect($strBanco))){
-                        print "Num deu boa";
-                        echo "ERRRORRR";
-                }else{
-
-                        $sql = "SELECT * FROM inv_srv";
-                        $result = pg_query($conexao, $sql);
-                        $resultAll = pg_fetch_all($result);
-
-                        echo "<table>";
-			echo "<th colspan = " . "2 " ."></th>";
-			echo "<tr>
-					<th>Hostname |</th>
-					<th>IP</th>
-				</tr>";
-
-                        foreach($resultAll as $key){
-                                echo "<tr>";
-                                echo "<td>" . $key['hostname'] . " |</td>";
-                                echo "<td>" . $key['ip'] . "</td>>";
-                                echo "</tr>";
-                        }
-                        echo "</table>";
-
-                        pg_close($conexao);
-
-                }
-
-        ?>
-
-
-
-
-</body>
-</html>
-
-/* se quiser ja me ajudar a colocar os campos dentro da tabela, eu agradeço hahahahaha
-a tabela ficara assim:
-
-hostname | IP
-XXXXXXXX | XXXXXX
-
-vlw 
-
-*/
+?>
