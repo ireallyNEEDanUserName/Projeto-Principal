c596ae6f42c8a4e6eed897e79b032839a165670f
Felipe Wieler <felipe.a.wieler@gmail.com>
2018-03-01 22:07:54 -0300

    Update main.js

--- v3.0/html/plataforma/main.js
+++ v3.0/html/plataforma/main.js
@@ -3,7 +3,7 @@
 var gameLoop = function(){
 
 	var canvas = document.getElementById("screen");
-	canvas.width = 700;
+	canvas.width = 480;
 	canvas.height = 500;
 	var screen = canvas.getContext("2d");
 	
@@ -12,73 +12,125 @@ var gameLoop = function(){
 	
 	var teclado = new Keyboarder();
 	
-	var posx = canvas.width / 2;
-	var posy = canvas.height - 64;
+	var pPos = {x: canvas.width / 2, y: canvas.height - 64};
 	var pos;
 	
-	var cameraPosX = 0;
 	var cameraPosY = 0;
 	
 	var player = new Image();
 	var plataforma = new Image();
 	player.src = "player/euspriteD0.png";
 	plataforma.src = "plataforma/plataforma.png";
-	var plataformaPosY = canvas.height - 32;
+	
+	var posicoesPlataforma = [{x: 0, y: canvas.height - 32},
+								{x: 160, y: canvas.height - 32},
+								{x: 160 * 2, y: canvas.height - 32},
+								{x: 160 * 3, y: canvas.height - 32},
+								{x: 160 * 4, y: canvas.height - 32}];
+								
+	var maiorY = 468;
+	
+	for(var u = 0; u < 100; u++){
+	
+		var add = true;
+		var randY = Math.floor((Math.random() * -5000) + 404) ;
+		var randX = Math.floor((Math.random() * canvas.width) - 32);
+		
+		for(var x = 0; x < posicoesPlataforma.length; x++){
+			if(posicoesPlataforma[x].y < 0 && randY < 0) var z = Math.abs(posicoesPlataforma[x].y) - Math.abs(randY);
+			else var z = posicoesPlataforma[x].y - randY;
+			if(z < 0) z *= -1;
+			if(z < 64){
+				add = false;
+				console.log("U: " + u + " PlatY: " + posicoesPlataforma[x].y + " randY: " + randY);
+			}
+		}
+		if(add){
+			posicoesPlataforma.push({x: randX, y: randY});
+		}
+	}
+	console.log(posicoesPlataforma);
 	
 	var direcao = "D"
 	var anterior = 0;
 	var anteriorJump = 0;
 	var jump = 0;
 	
+	var colidiu = collide(pPos, posicoesPlataforma);
+	
+	var subirCamera = 0;
+	var descerCamera = 0;
+	
 	var tick = function(){
 		
 		screen.fillRect(0, 0, canvas.width, canvas.height);
-		screen.drawImage(player, posx + cameraPosX, posy + cameraPosY);
+		screen.drawImage(player, pPos.x, pPos.y - cameraPosY);
 		
-		//PLATAFORMA DO CHÃO.
-		screen.drawImage(plataforma, 0 - cameraPosX, plataformaPosY - cameraPosY);
-		screen.drawImage(plataforma, plataforma.width - cameraPosX, plataformaPosY - cameraPosY);
-		screen.drawImage(plataforma, (plataforma.width * 2) - cameraPosX, plataformaPosY - cameraPosY);
-		screen.drawImage(plataforma, (plataforma.width * 3) - cameraPosX, plataformaPosY - cameraPosY);
-		screen.drawImage(plataforma, (plataforma.width * 4) - cameraPosX, plataformaPosY - cameraPosY);
-		//PLATAFORMA NO AR.
-		screen.drawImage(plataforma, (plataforma.width) - cameraPosX, plataformaPosY - cameraPosY - 160);
-		screen.drawImage(plataforma, (plataforma.width * 3) - cameraPosX, plataformaPosY - cameraPosY - 192);
-		
+		//PLATAFORMA.
+		for(var u = 0; u < posicoesPlataforma.length; u++){
+			screen.drawImage(plataforma, posicoesPlataforma[u].x, posicoesPlataforma[u].y - cameraPosY);
+		}		
 		anteriorJump = anterior;
 		[pos, direcao, anterior] = move(teclado, anterior, direcao, count);
 		
-		//FAZER A TELA SE MOVER PARA O LADO CONFORME SE MOVIMENTA
-		posx += pos.x;
+		pPos.x += pos.x;
 		
-		if(jump >= 0 && posy == canvas.height - 64) jump = pos.y;
-		else if(jump < 0){
-			console.log(jump);
-			posy -= 2;
-			cameraPosY -= 3;
-			jump += 5;
+		if(colidiu){
+			jump = pos.y;
+			//subirCamera += jump * -1;
 		}
 		
-		if(posy != canvas.height - 64) anterior = anteriorJump;
+		if(jump < 0){
+			pPos.y -= 5;
+			jump += 5;
+			subirCamera += 5;
+		}
+		
+		if(!colidiu) anterior = anteriorJump;
 		player.src = "player/eusprite" + direcao + anterior + ".png";
 		
 		if(count <= 5) count++;
 		else count = 0;
 		
-		if(posy < canvas.height - 64 && jump >= 0){
-			posy += 2;
-			cameraPosY += 3;
+		if(jump >= 0 && !colidiu){
+			pPos.y += 5;
+			descerCamera += 5;
 		}
 		
-		if(posy  > canvas.height - 64) posy = canvas.height - 64;
 		
-		p.innerHTML = count;
+		if(subirCamera > 0){
+			cameraPosY -= 3;
+			subirCamera -= 3;
+		}else if(descerCamera > 0){
+			cameraPosY += 3;
+			descerCamera -= 3;
+		}
+		
+		colidiu = collide(pPos, posicoesPlataforma);
+		if(colidiu) jump = 0;
+		
+		p.innerHTML = cameraPosY * -1;
+		
+		console.log(colidiu + " p:" + (pPos.y + 32) + " j:" + jump + " camPs: " + subirCamera);
 		
 		requestAnimationFrame(tick);
 	}
 	
 	tick();
 	
+};
+
+var collide = function(posPlayer, array){
+	
+	var colidiu = false;
+	for(var x = 0; x < array.length; x++){
+		
+		if((posPlayer.y + 32 >= array[x].y && posPlayer.y + 32 <= array[x].y + 32) &&
+			(posPlayer.x + 32 >= array[x].x && posPlayer.x + 32 <= array[x].x + 160)) colidiu = true;
+			
+	}
+	
+	return colidiu;
 };
 
 
